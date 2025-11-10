059a7b0d4a4e58c30c0f38cd102918c1a178cf5f
Jonathan <221150052@atitalaquia.tecnm.mx>
2025-11-10 08:20:39 -0600

    I added Google Analitycs

--- app.py
+++ app.py
@@ -18,6 +18,24 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+# --- INICIO: GOOGLE TAG MANAGER ---
+GTM_ID = "GTM-KVFNBHQ2"
+
+gtm_script = f"""
+    <script>(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':
+    new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    }})(window,document,'script','dataLayer','{GTM_ID}');</script>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={GTM_ID}"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    """
+
+# Usa components.html para inyectar ambos scripts
+components.html(gtm_script, height=0)
+# --- FIN: GOOGLE TAG MANAGER ---
+
 # --- CONEXIÓN A LA BASE DE CONOCIMIENTO (FIREBASE) ---
 @st.cache_resource
 def init_firebase():
